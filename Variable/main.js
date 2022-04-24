83523a65a9c667c0ff81ded7eb33c8d5b84cf95a
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-04-24 13:48:07 +0700

    Variable là biến , chứa một dữ liệu trong bộ nhớ ram
    Variable là biến , chứa một dữ liệu trong bộ nhớ ram

---
+++ Variable/main.js
@@ -0,0 +1,10 @@
+// Khai báo biến
+var MyName = 'Trần Trọng Hòa';
+var Locationz = "Vũng tàu";
+var Age = 14;
+var Skill = "HTML/CSS/JS/C#/PHP/PASCAL/PYTHON...";
+// In biến ra bên ngoài bằng alert
+alert("Tên tôi là: "+MyName);
+alert("Tôi ở: "+Locationz);
+alert("Tôi "+Age+" tuổi!");
+alert("Kĩ năng: "+Skill);
