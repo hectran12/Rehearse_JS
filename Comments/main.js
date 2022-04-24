50ff8b5ae1549eed4d6b1afea29fe58beb0c107d
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-04-24 13:49:06 +0700

    Comment , là ghi chú
    Thường dùng để comment lại những đoạn khó nhớ , hoạc comment nhưng code thừa thải. Phù hợp cho làm việc nhóm.

---
+++ Comments/main.js
@@ -0,0 +1,13 @@
+// Đây là một comment căn bản
+// alert("Khỏi thông báo");
+/**
+ * Multi comment
+ * Comment nhiều dòng
+ */
+
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
+// alert("Đang comment bỗng thấy mình quá là đẹp trai");
