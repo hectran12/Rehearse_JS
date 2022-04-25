d6b4bb1f0455ae230bb3fb0bfe5096fbbd132a93
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-04-25 18:05:26 +0700

    Toán tử gán
    Toán tử gán

---
+++ Assign_Operator/main.js
@@ -0,0 +1,34 @@
+// Toán tử gán
+var a = 5;
+
+// += 
+a += 2;
+console.log(a);
+// -=
+a -= 2;
+console.log(a);
+// *=
+a *= 5;
+console.log(a);
+// /= 
+a /= 5;
+console.log(a);
+// **=
+a **= 5;
+console.log(a);
+
+// Hậu tố
+var numberA = 5;
+numberA++;
+console.log(numberA);
+
+numberA--;
+console.log(numberA);
+
+// Tiền tố
+var numberB = 6;
+console.log(++numberB);
+console.log(numberB);
+
+console.log(--numberB);
+console.log(numberB);
