438ab1a1cc5fccb45e0d4f8d0a4b64979cac0f5c
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-04-28 22:22:59 +0700

    Toán tử so sánh
    Toán tử so sánh

---
+++ Comparison_Operator/main.js
@@ -0,0 +1,32 @@
+// Toán tử so sánh
+var itemA = "Con chó";
+var itemB = "Con mèo";
+
+// ==
+if(itemA == itemB){
+    console.log("ko có khả năng")
+} else {
+    console.log("Có khả năng")
+}
+// != 
+if(itemA != itemB){
+    console.log("ko có khả năng")
+} else {
+    console.log("Có khả năng")
+}
+// >
+var numA = 5;
+var numB = 6;
+if(numA > numB){
+    console.log("A lớn hơn B");
+} else {
+    console.log("A không lớn hơn B");
+}
+// <
+var numA = 5;
+var numB = 6;
+if(numA < numB){
+    console.log("A nhỏ hơn B");
+} else {
+    console.log("A lớn hơn B");
+}
