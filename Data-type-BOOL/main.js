291fc86be89f318c8fd9819c5e0fe6e4d7c415ad
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-07-07 15:49:42 +0700

    Delete Data-type-BOOL directory

--- Data-type-BOOL/main.js
+++
@@ -1,9 +0,0 @@
-// kIỂU DỮ LIỆU BOOL LÀ KIỂU DỮ LIỆU TRUE OR FALSE;
-var isTrue = true;
-console.log(isTrue);
-var isFalse = false;
-console.log(isFalse);
-var soSanh = 5 < 4; // false
-console.log(soSanh);
-var soSanh = 4 < 5; // true
-console.log(soSanh);
