d036afa9f33bb349f89421312584acb690313426
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-07-18 15:43:16 +0700

    Add files via upload

---
+++ Javascript_Basic/Bai59 - Module/kethua.js
@@ -0,0 +1,10 @@
+var hello = () => {
+    return "Hello world";
+}
+
+function outputFullName (firstName, lastName) {
+    return firstName + ' ' + lastName; 
+}
+ export {
+    hello, outputFullName
+ }
