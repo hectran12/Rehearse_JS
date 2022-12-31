16cff0891a5118002f551e145b864792e1cdabfd
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-12-31 08:10:27 +0700

    Update index.js

--- module/limitrq/index.js
+++ module/limitrq/index.js
@@ -1,7 +1,7 @@
 const rq = require('./limitrq');
 
 
-if (rq.checkRequest ('zzj')) {
+if (rq.checkRequest ('hoadeptrai_or_ipv4')) {
   console.log('Thực hiện request thành công');
 } else {
   console.log(`Bạn đã bị chặn, vì giới hạn ${rq.info()['requestperLimit']} requests/${rq.info()['limit_seconds'] / 60} phút`)
